fcb7eafebfccf29d08607046b5af6e4c281d298a
gdi2290 <github@gdi2290.com>
2013-10-17 13:07:32 -0700

    init tests

---
+++ test/angular-websocket_test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports['angularWebSocket'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(1, 1, 'pass the test.');
+    test.done();
+  }
+};
