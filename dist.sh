53a0a0af31909ef946fbcc5fc2a6b642ccb153c2
gdi2290 <github@gdi2290.com>
2014-12-29 00:21:54 -0800

    chore(dist): uglifyjs options

--- dist.sh
+++ dist.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 mkdir -p dist/
 
+echo "* Copy files"
 cat src/angular-websocket.js > dist/angular-websocket.js
 cat src/angular-websocket-mock.js > dist/angular-websocket-mock.js
 
-./node_modules/.bin/uglifyjs src/angular-websocket.js > dist/angular-websocket.min.js
+echo "dist/angular-websocket.js"
+echo "dist/angular-websocket-mock.js"
+echo
 
-echo "* built source files \ndist/angular-websocket.js\ndist/angular-websocket.min.js\ndist/angular-websocket-mock.js\n"
+echo "* Build source files"
+
+./node_modules/.bin/uglifyjs src/angular-websocket.js > dist/angular-websocket.min.js \
+--source-map dist/angular-websocket.min.js.map \
+--source-map-url angular-websocket.min.js.map \
+--mangle \
+--compress \
+--stats
+
+echo
+echo "dist/angular-websocket.min.js"
+echo "dist/angular-websocket.min.js.map"
+echo
