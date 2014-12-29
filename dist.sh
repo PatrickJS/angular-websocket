8aad47589c288e25c339a8d38f39dde174bce824
gdi2290 <github@gdi2290.com>
2014-12-28 22:46:41 -0800

    chore: allow test.sh/dist.sh with npm

--- dist.sh
+++ dist.sh
@@ -4,6 +4,6 @@ mkdir -p dist/
 cat src/angular-websocket.js > dist/angular-websocket.js
 cat src/angular-websocket-mock.js > dist/angular-websocket-mock.js
 
-uglifyjs src/angular-websocket.js > dist/angular-websocket.min.js
+./node_modules/.bin/uglifyjs src/angular-websocket.js > dist/angular-websocket.min.js
 
-echo "* built source files \ndist/angular-websocket.js\ndist/angular-websocket.min.js\ndist/angular-websocket-mock.js"
+echo "* built source files \ndist/angular-websocket.js\ndist/angular-websocket.min.js\ndist/angular-websocket-mock.js\n"
