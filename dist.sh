06daa5de56f9e94b8abcda19d4bf49123ae70510
gdi2290 <github@gdi2290.com>
2015-02-22 11:18:38 -0800

    chore(dist): build browserify example

--- dist.sh
+++ dist.sh
@@ -11,12 +11,20 @@ echo
 
 echo "* Build source files"
 
-./node_modules/.bin/uglifyjs src/angular-websocket.js > dist/angular-websocket.min.js \
+./node_modules/.bin/uglifyjs \
+src/angular-websocket.js > dist/angular-websocket.min.js \
 --source-map dist/angular-websocket.min.js.map \
 --source-map-url angular-websocket.min.js.map \
 --mangle \
 --compress \
 --stats
+
+./node_modules/.bin/browserify \
+--debug \
+-t browserify-ngannotate \
+./example/browserify-example/app/app.js \
+--outfile \
+./example/browserify-example/bundle.js
 
 echo
 echo "dist/angular-websocket.min.js"
