d92fe86229aa7bdf5af8210464443d2d9fae6dab
Amgad Naiem <amgad@peerialism.com>
2016-04-05 13:46:47 +0300

    fixed arraybuffer data type

--- dist/angular-websocket.js
+++ dist/angular-websocket.js
@@ -54,6 +54,7 @@
       this.initialTimeout              = options && options.initialTimeout             || 500; // 500ms
       this.maxTimeout                  = options && options.maxTimeout                 || 5 * 60 * 1000; // 5 minutes
       this.reconnectIfNotNormalClose   = options && options.reconnectIfNotNormalClose  || false;
+      this.binaryType                  = options && options.binaryType                 || 'blob';
 
       this._reconnectAttempts = 0;
       this.sendQueue          = [];
@@ -113,6 +114,7 @@
         this.socket.onopen  = angular.bind(this, this._onOpenHandler);
         this.socket.onerror = angular.bind(this, this._onErrorHandler);
         this.socket.onclose = angular.bind(this, this._onCloseHandler);
+        this.socket.binaryType = this.binaryType;
       }
     };
 
@@ -121,7 +123,7 @@
         var data = this.sendQueue.shift();
 
         this.socket.send(
-          isString(data.message) ? data.message : JSON.stringify(data.message)
+          isString(data.message) || this.binaryType != "blob" ? data.message : JSON.stringify(data.message)
         );
         data.deferred.resolve();
       }
