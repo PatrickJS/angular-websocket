31f15ab2436316f4bb64c3555c29d384e1cf037b
otaviodecampos <otaviodecampos@hotmail.com>
2016-04-09 19:05:58 -0300

    Added support for promise mock

--- src/angular-websocket.js
+++ src/angular-websocket.js
@@ -287,7 +287,14 @@
 
       if ($websocketBackend.isMocked && $websocketBackend.isMocked() &&
               $websocketBackend.isConnected(this.url)) {
-        this._onMessageHandler($websocketBackend.mockSend());
+        var mockSend = $websocketBackend.mockSend();
+        if(mockSend.then) {
+          mockSend.then(function(value) {
+            self._onMessageHandler(value);
+          });
+        } else {
+          self._onMessageHandler(mockSend);
+        }
       }
 
       return promise;
